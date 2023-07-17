d44207aed503d6618ee75d136bcd99f795262a19
Syed Mazhar <hussain_mazher@yahoo.com>
2023-07-17 09:43:17 +0530

    Initial commit with my customizations

---
+++ app/src/main/java/com/gucodero/biometric_compose/MainActivity.kt
@@ -0,0 +1,282 @@
+package com.gucodero.biometric_compose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.FragmentActivity
+import com.gucodero.biometric_compose.entities.AllowedAuthenticators
+import com.gucodero.biometric_compose.entities.CiphertextWrapper
+import com.gucodero.biometric_compose.entities.DecryptionResult
+import com.gucodero.biometric_compose.entities.EncryptionResult
+import com.gucodero.biometric_compose.utils.isBiometricPromptIsAvailable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val prefs = PreferenceUtil(baseContext)
+        setContent {
+            Screen(
+                prefs = prefs,
+                toast = ::toast,
+            )
+        }
+    }
+
+    private fun toast(msg: String){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+@Composable
+fun Screen(
+    prefs: PreferenceUtil,
+    toast: (String) -> Unit
+){
+    val isBiometricPromptAvailable = isBiometricPromptIsAvailable(
+        allowedAuthenticators = AllowedAuthenticators.BIOMETRIC_STRONG
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                MaterialTheme.colors.background
+            )
+            .padding(
+                16.dp
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if(!isBiometricPromptAvailable){
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.biometric_not_available)
+                )
+            }
+        } else {
+            val biometricPrompt = rememberBiometricPrompt(
+                allowedAuthenticators = AllowedAuthenticators.BIOMETRIC_STRONG,
+                title = stringResource(id = R.string.biometric_prompt_title),
+                subtitle = stringResource(id = R.string.biometric_prompt_subtitle),
+                description = stringResource(id = R.string.biometric_prompt_description),
+                negativeButtonText = stringResource(id = R.string.biometric_prompt_negative_button),
+                confirmationRequired = false
+            )
+            val coroutine = rememberCoroutineScope()
+            var key by remember {
+                mutableStateOf(prefs.username)
+            }
+            var value by remember {
+                mutableStateOf("")
+            }
+
+            var savedCipher: CiphertextWrapper? = null
+            if (prefs.useFingerprint) {
+                savedCipher = CiphertextWrapper(prefs.cipherText, prefs.initializationVector)
+                LaunchedEffect(key1 = Unit) {
+                    coroutine.launch {
+                        biometricPrompt.decryption(
+                            key = prefs.username,
+                            value = savedCipher!!
+                        ).collect {
+                            when (it) {
+                                is DecryptionResult.Success -> {
+                                    toast("SUCCESS: ${it.value}")
+                                    value = it.value
+                                }
+
+                                is DecryptionResult.Failed -> {
+                                    toast("FAILED")
+                                }
+
+                                is DecryptionResult.Error -> {
+                                    toast("ERROR: ${it.errorCode} - ${it.description}")
+                                }
+
+                                is DecryptionResult.Cancel -> {
+                                    toast("CANCEL")
+                                }
+
+                                is DecryptionResult.NotAvailable -> {
+                                    toast("NotAvailable")
+                                }
+
+                                is DecryptionResult.CipherException -> {
+                                    toast(it.exception.toString())
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            var cipherValue: CiphertextWrapper? by remember {
+                mutableStateOf(savedCipher)
+            }
+            if(cipherValue == null){
+                TextField(
+                    value = key,
+                    onValueChange = {
+                        key = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = "KEY")
+                    }
+                )
+                TextField(
+                    value = value,
+                    onValueChange = {
+                        value = it
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp),
+                    label = {
+                        Text(text = "VALUE")
+                    }
+                )
+                Button(
+                    onClick = {
+                        coroutine.launch {
+                            biometricPrompt.encryption(
+                                key = key,
+                                value = value
+                            ).collect {
+                                when(it){
+                                    is EncryptionResult.Success -> {
+                                        toast("SUCCESS")
+                                        cipherValue = it.cipherValue
+                                        /*prefs.username = key
+                                        prefs.cipherText = it.cipherValue.ciphertext
+                                        prefs.initializationVector = it.cipherValue.initializationVector
+                                        prefs.useFingerprint = true*/
+                                        saveCredentials(prefs, key, it.cipherValue)
+                                    }
+                                    is EncryptionResult.Failed -> {
+                                        toast("FAILED")
+                                    }
+                                    is EncryptionResult.Error -> {
+                                        toast("ERROR: ${it.errorCode} - ${it.description}")
+                                    }
+                                    is EncryptionResult.Cancel -> {
+                                        toast("CANCEL")
+                                    }
+                                    is EncryptionResult.NotAvailable -> {
+                                        toast("NotAvailable")
+                                    }
+                                    is EncryptionResult.CipherException -> {
+                                        toast(it.exception.toString())
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp),
+                    enabled = key.isNotEmpty() && value.isNotEmpty()
+                ) {
+                    Text(text = "Encryption")
+                }
+            }
+            else {
+                TextField(
+                    value = key,
+                    onValueChange = {
+                        key = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = "KEY")
+                    }
+                )
+                Button(
+                    onClick = {
+                        coroutine.launch {
+                            biometricPrompt.decryption(
+                                key = key,
+                                value = cipherValue!!
+                            ).collect {
+                                when(it){
+                                    is DecryptionResult.Success -> {
+                                        toast("SUCCESS: ${it.value}")
+                                    }
+                                    is DecryptionResult.Failed -> {
+                                        toast("FAILED")
+                                    }
+                                    is DecryptionResult.Error -> {
+                                        toast("ERROR: ${it.errorCode} - ${it.description}")
+                                    }
+                                    is DecryptionResult.Cancel -> {
+                                        toast("CANCEL")
+                                    }
+                                    is DecryptionResult.NotAvailable -> {
+                                        toast("NotAvailable")
+                                    }
+                                    is DecryptionResult.CipherException -> {
+                                        toast(it.exception.toString())
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp),
+                    enabled = key.isNotEmpty()
+                ) {
+                    Text(text = "Decryption")
+                }
+                Button(
+                    onClick = {
+                        cipherValue = null
+                        key = ""
+                        value = ""
+                        resetCredentials(prefs)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp)
+                ) {
+                    Text(text = "Reset")
+                }
+            }
+        }
+    }
+}
+
+private fun saveCredentials(
+    prefs: PreferenceUtil,
+    key: String,
+    cipherValue: CiphertextWrapper
+) {
+    prefs.username = key
+    prefs.cipherText = cipherValue.ciphertext
+    prefs.initializationVector = cipherValue.initializationVector
+    prefs.useFingerprint = true
+}
+
+private fun resetCredentials(prefs: PreferenceUtil) {
+    prefs.username = ""
+    prefs.useFingerprint = false
+}
---
+++ app/src/main/java/com/gucodero/biometric_compose/PreferenceUtil.kt
@@ -0,0 +1,68 @@
+package com.gucodero.biometric_compose
+
+import android.content.Context
+
+
+/**
+ * Created by Mazhar on 16/07/23
+ */
+class PreferenceUtil(context: Context) {
+
+    private val sharedPreferences = context.getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+
+    var username: String
+        get() = sharedPreferences.getString(USERNAME, "") ?: ""
+        set(value) = sharedPreferences.edit().putString(USERNAME, value).apply()
+
+    var encryptedPassword: String
+        get() = sharedPreferences.getString(ENCRYPTED_PASSWORD, "") ?: ""
+        set(value) = sharedPreferences.edit().putString(ENCRYPTED_PASSWORD, value).apply()
+
+    var cipherIv: String
+        get() = sharedPreferences.getString(CIPHER_IV, "") ?: ""
+        set(value) = sharedPreferences.edit().putString(CIPHER_IV, value).apply()
+
+    var useFingerprint: Boolean
+        get() = sharedPreferences.getBoolean(USE_FINGERPRINT, false)
+        set(value) = sharedPreferences.edit().putBoolean(USE_FINGERPRINT, value).apply()
+
+    var cipherText: ByteArray
+        get() {
+            val str = sharedPreferences.getString(CIPHER_TEXT, "") ?: ""
+            return str.toPreservedByteArray
+        }
+        set(value) {
+            val str = value.toPreservedString
+            sharedPreferences.edit().putString(CIPHER_TEXT, str).apply()
+        }
+
+    var initializationVector: ByteArray
+        get() {
+            val str = sharedPreferences.getString(IV, "") ?: ""
+            return str.toPreservedByteArray
+        }
+        set(value) {
+            val str = value.toPreservedString
+            sharedPreferences.edit().putString(IV, str).apply()
+        }
+
+
+    companion object {
+        private const val USERNAME = "username"
+        private const val ENCRYPTED_PASSWORD = "encrypted_password"
+        private const val CIPHER_IV = "cipher_iv"
+        private const val USE_FINGERPRINT = "use_fingerprint"
+        private const val CIPHER_TEXT = "cipher_text"
+        private const val IV = "initialization_vector"
+    }
+
+    val String.toPreservedByteArray: ByteArray
+        get() {
+            return this.toByteArray(Charsets.ISO_8859_1)
+        }
+
+    val ByteArray.toPreservedString: String
+        get() {
+            return String(this, Charsets.ISO_8859_1)
+        }
+}
